41fa2dfea0b3c59006deed270e4c23075f5ac11c
Sergi <sgarcia030@gmail.com>
2018-05-18 00:04:49 +0200

    Added i3block scripts

--- I3/.bin/wifi-conection.sh
+++ I3/.bin/wifi-conection.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-if [[ -z "$(iwgetid -r)" ]];then
-    echo "Disconnected "
-else
-    echo "$(iwgetid -r) "
+
+if ! [ -z "$(find /lib/modules/$(uname -r)/kernel/drivers/net/wireless -name '*.ko')" ];then
+    if [[ -z "$(iwgetid -r)" ]];then
+        echo "Disconnected "
+    else
+        echo "$(iwgetid -r) "
+    fi
+
 fi
