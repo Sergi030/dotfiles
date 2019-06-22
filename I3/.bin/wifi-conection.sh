f3fca616140108c1b6e495a542b351cee1e51998
Sergi Garcia Ibañez <sgarcia030@gmail.com>
2019-06-22 14:24:23 +0200

    Adding wifi-connection thigns

--- I3/.bin/wifi-conection.sh
+++ I3/.bin/wifi-conection.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-if ! [ -z "$(find /lib/modules/$(uname -r)/kernel/drivers/net/wireless -name '*.ko')" ];then
-    if [[ -z "$(iwgetid -r)" ]];then
-        echo "Disconnected "
-    else
-        echo "$(iwgetid -r) "
-    fi
-
+if [[ -z "$(iwgetid -r)" ]];then
+    echo "Disconnected "
+else
+    echo "$(iwgetid -r) "
 fi
+
