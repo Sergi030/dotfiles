817191456afc790d328c687d634ae690838c4638
Sergi Garcia Ibañez <sgarcia@worldsensing.com>
2025-06-03 00:38:10 +0200

    bin to local/bin

--- I3/.local/bin/keyboard.sh
+++ I3/.local/bin/keyboard.sh
@@ -4,13 +4,13 @@ lang=$(setxkbmap -query | grep layout: | awk '{print $2}')
 
 if [ $lang == 'en' ]
 then
-    setxkbmap es
+    xkb-switch -s es
     dunstify -a 'Keyboard' -r 2593 -u normal "ES"
 elif [ $lang == 'es' ]  
 then
-    setxkbmap us
+    xkb-switch -s us
     dunstify -a 'Keyboard' -r 2593 -u normal "US"
 else 
-    setxkbmap es
+    xkb-switch -s es
     dunstify -a 'Keyboard' -r 2593 -u normal "ES"
 fi  
